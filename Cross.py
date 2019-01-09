71914d736862a0e267d4f99446630deb8f55608d
Prema Grandhi <pgrandhi@microsoft.com>
2019-01-09 12:14:12 -0800

    Turtle Drawing Python Samples
    Solving Homework 1 - Turtle Drawing problems.

---
+++ Cross.py
@@ -0,0 +1,10 @@
+import turtle
+
+turtle.forward(100)
+turtle.backward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(180)
+turtle.forward(100)
+
+
---
+++ hourglass.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.forward(100)
+turtle.left(240)
+turtle.forward(100)
+turtle.left(240)
+turtle.forward(100)
+turtle.backward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+
+
+
