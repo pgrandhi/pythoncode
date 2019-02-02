f316b646616860c73bec7ec97946d09b29ca854e
Prema Grandhi <pgrandhi@outlook.com>
2019-02-01 22:14:10 -0800

    Updated 4.DrawChessboard script
    Updated 4.DrawChessboard script

--- Assignment3/4.DrawChessboard.py
+++ Assignment3/4.DrawChessboard.py
@@ -1,6 +1,5 @@
 import turtle
 def drawGrid(startx, starty,width, height, color):
-    print("startx:", startx, "starty:",starty, "width:",width, "height:",height)
     turtle.penup()
     turtle.pencolor('black')
     turtle.fillcolor(color)
@@ -9,24 +8,27 @@ def drawGrid(startx, starty,width, height, color):
     turtle.begin_fill()
     for x in range(2):
         turtle.forward(width)
-        turtle.left(90)
+        turtle.right(90)
         turtle.forward(height)
-        turtle.left(90)
+        turtle.right(90)
     turtle.end_fill()
-    
-def drawChessboard(startx, endx, starty, endy):
-    width = (endx-startx)/8
-    height = (endy-starty)/8
-    for row in range(8):
-        for column in range(8):
+
+def drawChessboardByWidthAndHeight(startx, starty, width, height):
+    for row in range(0,8):
+        for column in range(0,8):
             if ((row+column) %2 ==0):
                 color='black'
-                drawGrid(startx + column * width, starty+row*height, width,height,color)
             else:
                 color='white'
-                drawGrid(startx + column * width, starty+row*height, width,height,color)
+            drawGrid(startx + column * width, starty + row*height, width, height,  color)
+
+            
+def drawChessboard(startx, starty, endx, endy):
+    width = (endx - startx)/8
+    height = (endy - starty)/8
+    drawChessboardByWidthAndHeight(startx, starty, width, height)
     
-    
-drawGrid(30,30,30, 30, 'black')
-drawChessboard(0,0,240,240)
+drawChessboard(-240, -180, 0, 60)
+drawChessboard(40, -180, 280, 60)
+turtle.done()
 
