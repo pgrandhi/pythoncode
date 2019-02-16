c7e942c1cb092915c6f1f4e5a8595357a3fa6030
Prema Grandhi <pgrandhi@outlook.com>
2019-02-16 11:32:21 -0800

    Updating notebook and testnotebook scripts from Feb 16th class
    Updating notebook and testnotebook scripts

--- Class/Notebook.py
+++ Class/Notebook.py
@@ -10,11 +10,14 @@ class Notebook:
     def __init__(self):
         self.__notes = []
 
+    def getNotes(self):
+        return self.__notes
+
     def new_note(self, memo, tags = ''):
         n = Note(memo, tags)
         self.__notes.append(n)
-
-    def modify_note(self,note_id,memo):
+        
+    def modify_memo(self,note_id,memo):
         n = self.__find_note(note_id)
         #if not n --> does not have value
         if not n:
@@ -22,19 +25,33 @@ class Notebook:
         else:
             n.setMemo(memo)
 
+    def modify_tags(self,note_id,tags):
+        n = self.__find_note(note_id)
+        #if not n --> does not have value
+        if not n:
+            print("Note not found. try again")
+        else:
+            n.tags = tags
+
     def print_note(self,note_id):
         n = self.__find_note(note_id)
         #if not n --> does not have value
         if not n:
             print("Note not found. try again")
         else:
-            print("note id:",n.getId(), "memo:",n.getMemo(), "tags:",n.tags)
+            print("{0}: {1} \n {2}".format(n.getId(),n.getMemo(), n.tags))
 
-    def match_note(self, keyword):
-        for note in self.__notes:
-            if note.match(keyword):
-                print("matching keyword:",keyword, "node id:",note.getId(), "memo:",note.getMemo(), "tags:",note.tags)
+    def search(self, filter):
+        '''
+        find all notes that match the given filter
+        :paramm filter - keyword to search on
+        return notes
     
+        for note in self.__notes:
+            if note.match(filter):
+                print("matching keyword:",keyword, "node id:",note.getId(), "memo:",note.getMemo(), "tags:",note.tags)
+        '''
+        return [note for note in self.__notes if note.match(filter)]
 
     def __find_note(self,note_id):
         for note in self.__notes:
--- Class/TestNotebook.py
+++ Class/TestNotebook.py
@@ -2,23 +2,76 @@
 #You have to specify from <file> import <class>.
 #Names are case-sensitive and it needs to match the filename and class name
 #You can also do from Note import *
+import sys
+from Notebook import Notebook,Note
 
-from Notebook import Notebook
+class Menu:
+    def __init__(self):
+        self.notebook = Notebook()
+        #dictionary - also called object in python and JS
+        self.choices = {
+            "1": self.show_notes,
+            "2": self.search_notes,
+            "3": self.add_note,
+            "4": self.modify_note,
+            "5": self.quit
+            }
+        
+    def displayMenu(self):
+        #to indent in the same way as type here, use """
+        print("""
+           Notebook Menu
 
-nb1 = Notebook()
-nb1.new_note("first note in notebook", "tag")
-nb1.new_note("second note in notebook", "second")
-nb1.new_note("third note in notebook", "third")
-nb1.print_note(1)
-nb1.print_note(2)
-nb1.print_note(3)
+            1. Show all notes
+            2. Search notes
+            3. Add note
+            4. Modify note
+            5. Quit
+            """)
 
-nb1.modify_note(2,"modified")
-nb1.print_note(1)
-nb1.print_note(2)
-nb1.print_note(3)
+    def run(self):
+        #run forever
+        while True:
+            self.displayMenu()
+            choice = input("Enter an option:")
+            action = self.choices.get(choice)
+            if action:
+                action()
+            else:
+                print("{0} is not a valid choice".format(choice))
 
-nb1.modify_note(4,"modified note where id does not exist")
+    def quit(self):
+        print("Thank you for using the notebook.")
+        sys.exit(0)
 
-nb1.match_note("tag")
-nb1.match_note("note")
+    def show_notes(self, notes = None):
+        if not notes:
+            notes = self.notebook.getNotes()
+        for note in notes:
+            self.notebook.print_note(note.getId())
+
+    def search_notes(self):
+        filter = input("search for:")
+        notes = self.notebook.search(filter)
+        show_notes(notes)
+
+    def add_note(self):
+        memo = input("Enter a memo:")
+        self.notebook.new_note(memo)
+        print("Your note has been added")
+
+    def modify_note(self):
+        noteId = int(input("Enter the note ID"))
+        memo = input("Enter a memo:")
+        tags = input("Enter tags:")
+        if memo:
+            self.notebook.modify_memo(noteId,memo)
+        if tags:
+          self.notebook.modify_tags(noteId,tags)
+
+menu = Menu()
+menu.run()
+          
+    
+
+    
